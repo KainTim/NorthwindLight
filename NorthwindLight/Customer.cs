7a0806d157078b31e0bdf8d4aa916aa335bac93e
Tim Kainz <tikaiz@gmx.at>
2025-03-12 15:08:09 +0100

    Add project files.

---
+++ NorthwindLight/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindLight;
+public class Customer
+{
+  public int Id { get; set; }
+  public double Latitude { get; set; }
+  public double Longitude { get; set; }
+  public string Name { get; set; } = null!;
+
+}
---
+++ NorthwindLight/NorthwindLightContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorthwindLight;
+
+public class NorthwindLightContext : DbContext
+{
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (optionsBuilder.IsConfigured)
+    {
+      Console.WriteLine("Is already Configured");
+      return;
+    }
+    string connectionString = @"data source=C:\\Databases\NorthwindLite.db";
+    Console.WriteLine($"Using connectionstring {connectionString}");
+    optionsBuilder.UseSqlite(connectionString);
+  }
+}
